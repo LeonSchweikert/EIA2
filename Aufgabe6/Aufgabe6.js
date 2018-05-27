56fd2c2fac07cacfc71f5b55d4b468471643b166
User <User@DESKTOP-GNESREK.Speedport_W_723V_1_45_000>
2018-05-27 14:24:10 +0200

    .

---
+++ Aufgabe6/Aufgabe6.js
@@ -0,0 +1,6 @@
+var StudiVZ;
+(function (StudiVZ) {
+    // assoziatives Array um eine Person mit Matrikel abzuspeichern
+    StudiVZ.studiHomoAssoc = {};
+})(StudiVZ || (StudiVZ = {}));
+//# sourceMappingURL=Aufgabe6.js.map
